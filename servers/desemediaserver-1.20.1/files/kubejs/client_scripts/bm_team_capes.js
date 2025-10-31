bd32ba75b9a29170c8138ce8b4527c8129fb4c13
Gawnul <gaelpsrobotica@gmail.com>
2025-10-31 16:48:02 +0100

    V1

---
+++ servers/desemediaserver-1.20.1/files/kubejs/client_scripts/bm_team_capes.js
@@ -0,0 +1,13 @@
+
+
+
+// BEYOND MINES TEAM MEMBER CAPE
+
+
+
+CapeJS.addCapes(event => {
+    event.register("016f7b43-1452-4912-be9e-4c6a78a99b26", "beyondmines_cape");			// Leonniclass
+	event.register("b487ecfc-424a-4318-85ea-f3a8aaaf3b3d", "beyondmines_cape");			// MastaTokki
+    event.register("5989dfb5-c121-4de5-b7e7-719d6f7d8b03", "beyondmines_cape");			// Qianson
+    event.register("b41a42f9-8445-4674-9d1e-6239bb18ef55", "beyondmines_cape");			// NAME	
+});
